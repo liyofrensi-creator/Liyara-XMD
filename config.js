755104cfdc0da1f4f930abd6160a4bb9c570af23
liyofrensi-creator <liyofrensi@gmail.com>
2026-09-08 15:17:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
     botName: "LIYARA BOT V1",
     ownerName: "☠️A4K☠️",
     // ඔයාට කැමති ලස්සන ඇනිමෙ හෝ බෝට් ෆොටෝ එකක ලින්ක් එක මෙතනට දාන්න පුළුවන්
-    menuImage: "https://i.ibb.co/BHF34z2k/file-00000000c5e8820abdf8d2fcba7b38b9.png", 
+    menuImage: "https://iili.io/n3j15EG.md.jpg", 
     version: "V1.0.0"
 };
