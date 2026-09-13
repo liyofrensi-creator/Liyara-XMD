5249b4012a7e40e99bf5f62295df186d780516c0
liyofrensi-creator <liyofrensi@gmail.com>
2026-09-13 20:53:33 +0530

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    botName: "LIYARA BOT V1",
-    ownerName: "☠️A4K☠️",
-    // ඔයාට කැමති ලස්සන ඇනිමෙ හෝ බෝට් ෆොටෝ එකක ලින්ක් එක මෙතනට දාන්න පුළුවන්
-    menuImage: "https://iili.io/n3j15EG.md.jpg", 
-    version: "V1.0.0"
-};
