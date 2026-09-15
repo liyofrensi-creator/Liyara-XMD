9c3698b5e10618ffac8136a48a59c34dd46281ca
liyofrensi-creator <liyofrensi@gmail.com>
2026-09-15 05:30:07 +0530

    Add geminiApiKey to BotConfig
    Added geminiApiKey to BotConfig interface and config object.

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,15 @@ interface BotConfig {
     ownerName: string;
     version: string;
     menuImage: string;
+    geminiApiKey: string;
 }
 
 const config: BotConfig = {
     botName: "LIYARA XMD",
     ownerName: "Liyo",
     version: "1.0.0",
-    menuImage: "https://i.postimg.cc/BvV0MGJ8/Whats-App-Image-2026-09-08-at-4-44-57-PM.jpg"
+    menuImage: "https://i.postimg.cc/BvV0MGJ8/Whats-App-Image-2026-09-08-at-4-44-57-PM.jpg",
+    geminiApiKey: process.env.GEMINI_API_KEY || ""
 };
 
 export default config;
